7c0b51ff219ec57ee6905258fad718695c32496d
Binh Nguyen <nbinh1995@gmail.com>
2020-07-30 10:21:03 +0700

    sev

---
+++ webservice/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Customer;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Customer::class, 10)->create();
+    }
+}
